292dd7f922aff074942ea3d5f30a07434d666f08
extorc <66661460+extorc@users.noreply.github.com>
2021-03-12 06:38:00 +0530

    Create Start.java

---
+++ e2/Start.java
@@ -0,0 +1,36 @@
+package glfwWindowTutorial.main;
+
+import javax.swing.JWindow;
+
+public class Start implements Runnable{
+	Thread gameThread;
+	Boolean running = true;
+	Window window;
+	public static void main(String[] args) {
+		new Start().start();
+	}
+	public void start() {
+		gameThread = new Thread(this, "Game");
+		gameThread.start();
+	}
+	public void run() {
+		init();
+		while(running == true) {
+			update();
+			render();
+			if(window.shouldclose()) {
+				running = false;
+			}
+		}
+	}
+	public void init() {
+		window = new Window(600,600,"Our Game");
+		window.createWindow();
+	}
+	public void update() {
+		window.update();
+	}
+	public void render() {
+		window.render();
+	}
+}
